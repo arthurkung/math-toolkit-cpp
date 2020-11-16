07283f9898677bd51b07781c78853e990f76ca02
Arthur Kung <arthurkung1006@gmail.com>
2020-11-16 22:06:45 +0000

    put fraction class to header file

---
+++ fractionClass/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Fraction.h"
+
+int main()
+{/*
+    Fraction a{ 1,6 };
+    Fraction b{ 1,3 };
+    std::cout << a << "\n";
+    std::cout << b << "\n";
+    std::cout << a + b << "\n";*/
+
+    Fraction c;
+    std::cout << "Enter a fraction: \n";
+    std::cin >> c;
+    std::cout << c << "\n";
+
+
+    return 0;
+}
