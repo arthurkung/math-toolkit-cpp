adbb0bfda5ed48863cfd157e3520aae54a7219c4
Arthur Kung <arthurkung1006@gmail.com>
2020-11-25 22:54:34 +0000

    other methods to pass in function pointer

--- LinearRegression/LinearRegression.cpp
+++ LinearRegression/LinearRegression.cpp
@@ -6,9 +6,14 @@ int main()
 {
     LinearRegressor lr;
 
-    lr.m_x = { 1,2,3,4,5 };
-    lr.m_y = { 2,5,8,11,14 };
-    std::cout << lr.getConstTerm() << "\n";
-    std::cout << lr.getGradientWithConstTerm() << "\n";
-    std::cout << lr.getGradientWithoutConstTerm() << "\n";
+    //lr.m_x = { 1,2,3,4,5 };
+    //lr.m_y = { 2,5,8,11,14 };
+    //std::cout << lr.getConstTerm() << "\n";
+    //std::cout << lr.getGradientWithConstTerm() << "\n";
+    //std::cout << lr.getGradientWithoutConstTerm() << "\n";
+
+    std::vector<double> hours{ 0.5,0.75,1.0,1.25,1.5,1.75,1.75,2.0,2.25,2.5,2.75,3.0,3.25,3.5,4.0,4.25,4.5,4.75,5.0,5.5 };
+    std::vector<double> pass{ 0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0,1.0,1.0,1.0,1.0,1.0,1.0};
+    
+
 }
--- equationSolver/main.cpp
+++ equationSolver/main.cpp
@@ -6,6 +6,19 @@ double simpleLinear(double x) { return 1.0 - 3.0 * x; }
 int main()
 {
     //funcPtr f{&simpleLinear};
+    //EquationSolver solver(&f);
+
+
+    //EquationSolver solver([](double x) -> double {
+    //    return 1.0 - 3.0 * x;
+    //    });
+
+    //auto linear{
+    //    [](double x) {return 1.0 - 3.0 * x;}
+    //};
+    //EquationSolver solver (linear);
+
+
     EquationSolver solver(&simpleLinear);
     std::cout << solver.evaluateFunc(2.0)<<"\n";
 }
